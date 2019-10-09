0311d199e51d006add641204becda6a3536a14f7
Kaden Neuman <kadenn2019@gmail.com>
2019-10-08 17:13:44 -0700

    first draft

--- arrays.js
+++ arrays.js
@@ -14,42 +14,97 @@ var instructors = [
     { firstname : 'James', teaches : 'Ruby'}
 ];
 
-
+console.log(teach(instructors))
 // ---------------------------
 // 1. Find largest number
 // ---------------------------
+ function largest(arr){
+     return Math.max(...arr)
+ }
 
-
+console.log(largest(numbers))
 // ---------------------------
 // 2. Find longest string
 // ---------------------------
+function length(arr){
+    var lngth = 0;
+    var longest;
+    for(var i =0; i < arr.length; i ++){
+        if (arr[i].length > lngth){
+            lngth = arr[i].length;
+            longest = arr[i]
+        }
+        
+    }
+    return longest;
+}
 
-
+console.log(length(strings))
 // ---------------------------
 // 3. Find even numbers
 // ---------------------------
 
+function even(arr){
+    var a = []
+    for(var i =0; i < arr.length; i ++){
+        if (arr[i] % 2 === 0){
+            a.push(arr[i])
+        }
+    }
+return a;
+}
 
-
+console.log(even(numbers))
 // ---------------------------
 // 4. Find odd numbers
 // ---------------------------
 
+function odd(arr){
+    var a = []
+    for(var i =0; i < arr.length; i ++){
+        if (arr[i] % 2 === 1){
+            a.push(arr[i])
+        }
+    }
+return a;
+}
 
+console.log(odd(numbers))
 // ---------------------------
 // 5. Find words that contain `is`
 // ---------------------------
 
+function is(arr){
+    var a = []
+    for(var i =0; i < arr.length; i ++){
+        if (arr[i].includes("is")){
+            a.push(arr[i])
+        }
+    }
+return a;
+}
 
+console.log(is(strings))
 
 // ---------------------------
 // 5. Join Both Arrays Together
 // ---------------------------
 
-
+console.log(numbers.concat(strings));
 
 
 // ---------------------------
 // 6. Use the Instructors array and find all that teach JavaScript, 
 //    then sort them alphabetically
 // ---------------------------
+
+function teach(arr){
+    var a = [];
+    for(var i =0; i < arr.length; i ++){
+        if (arr[i].teaches.includes('JavaScript')){
+            a.push(arr[i].firstname);
+        }
+    }
+    return a.sort();
+}
+
--- excercises-2.js
+++ excercises-2.js
@@ -4,6 +4,11 @@
 // if-then-else construct available in JavaScript.
 // ---------------------
 
+function max(a, b){
+    if (a < b)
+    return b
+}
+
 console.assert(max(2,3) === 3)
 console.assert(max(23,24) === 24)
 console.assert(max(-23, 12) === 12)
@@ -12,6 +17,17 @@ console.assert(max(-23, 12) === 12)
 // Define a function maxOfThree() that takes three 
 // numbers as arguments and returns the largest of them.
 // ---------------------
+
+function maxOfThree(a, b, c){
+    if (a>b && a>c) {
+        return a
+    }else if (b>a && b>c){
+        return b
+    }else if (c>a && c>b){
+        return c
+    }
+
+}
 
 console.assert(maxOfThree(2,56,3) === 56)
 console.assert(maxOfThree(12,3,4) === 12)
@@ -22,6 +38,14 @@ console.assert(maxOfThree(-12,4,-5) === 4)
 // string of length 1) and returns true if it is a 
 // vowel, false otherwise.
 // ---------------------
+
+function isVowel(str){
+    if (str == "a" || str == "E"){
+        return true
+    } else{
+        return false
+    } 
+}
 
 console.assert(isVowel(0) === false);
 console.assert(isVowel("B") === false);
@@ -38,19 +62,36 @@ console.assert(isVowel("2") === false)
 // the string "tothohisos isos fofunon".
 // ---------------------
 
+function rovarspraket(str){
+    var a = "";
+    if (str == 0) {
+        return str.toString();
+    }
+    for(var i =0; i < str.length; i ++) {
+        if(str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u"){
+            a = a + str[i];
+        }else {
+            a = a + str[i] + "o" + str[i];
+        }
+    }
+    return a;
+}
+
 console.assert(rovarspraket("a") === "a")
 console.assert(rovarspraket("b") === "bob")
 console.assert(rovarspraket("cat") === "cocatot")
 console.assert(rovarspraket("javascript") === "jojavovasoscocroripoptot")
 console.assert(rovarspraket(0) === "0")
 
-
 // ---------------------
 // Define a function reverse() that computes the reversal 
 // of a string. For example, reverse("jag testar") should 
 // return the string "ratset gaj".
 // ---------------------
-
+ 
+function reverse(str){
+    return str.split("").reverse().join("");
+}
 
 console.assert(reverse("books") === "skoob")
 console.assert(reverse("we don't want no trouble") === "elbuort on tnaw t'nod ew")
